27e4df3c86c6f0603cc110837088928aeee6cffa
Oleksandr Pyliavets <Oleksandr Pyliavets@DESKTOP-7JRVHFS>
2024-11-17 16:19:10 +0200

    Add project files.

---
+++ HW 4.2/HW 4.2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+Console.Write("Enter first number: ");
+double firstNumber = double.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+double secondNumber = double.Parse(Console.ReadLine());
+Console.Write("Enter operation (-, +, *, /): ");
+char operation = Console.ReadKey().KeyChar;
+double result;
+Console.WriteLine();
+Console.Write("Result: ");
+
+switch (operation)
+{
+    case '-':
+        result = firstNumber - secondNumber;
+        Console.WriteLine(firstNumber - secondNumber);
+        break;
+    case '+':
+        result = firstNumber + secondNumber;
+        Console.WriteLine(firstNumber + secondNumber);
+        break;
+    case '*':
+        result = firstNumber * secondNumber;
+        Console.WriteLine(firstNumber * secondNumber);
+        break;
+    case '/':
+        if (secondNumber == 0) // Виключення ділення на нуль
+            Console.WriteLine("Error: division by zero");
+        else
+            Console.WriteLine(firstNumber / secondNumber);
+        break;
+    default:
+        Console.WriteLine("Error: unknown input");
+        break;
+}
+

bfeb35206e3b2b102ce387a631a9e06caf6934c4
Oleksandr Pyliavets <Oleksandr Pyliavets@DESKTOP-7JRVHFS>
2024-11-20 14:41:39 +0200

    Format exception

--- HW 4.2/HW 4.2.cs
+++ HW 4.2/HW 4.2.cs
@@ -1,42 +1,45 @@
 ﻿using System;
 
+try
+{
 Console.Write("Enter first number: ");
 int firstNumber = int.Parse(Console.ReadLine());
 Console.Write("Enter second number: ");
 int secondNumber = int.Parse(Console.ReadLine());
 Console.Write("Enter operation (-, +, *, /): ");
 char operation = Console.ReadKey().KeyChar;
-double result;
+int result;
 Console.WriteLine();
 Console.Write("Result: ");
 
-switch (operation)
-{
-    case '-':
-        result = firstNumber - secondNumber;
-        Console.WriteLine(firstNumber - secondNumber);
-        break;
-    case '+':
-        result = firstNumber + secondNumber;
-        Console.WriteLine(firstNumber + secondNumber);
-        break;
-    case '*':
-        result = firstNumber * secondNumber;
-        Console.WriteLine(firstNumber * secondNumber);
-        break;
-    case '/':
-        try
-        {
-            result = firstNumber / secondNumber;
-            Console.WriteLine(firstNumber / secondNumber);
-        }
-        catch (DivideByZeroException)
-        {
-            Console.WriteLine("Division by zero");
-        }
-        break;
-    default:
-        Console.WriteLine("Error: unknown input");
-        break;
+    switch (operation)
+    {
+        case '-':
+            Console.WriteLine(firstNumber - secondNumber);
+            break;
+        case '+':
+            Console.WriteLine(firstNumber + secondNumber);
+            break;
+        case '*':
+            Console.WriteLine(firstNumber * secondNumber);
+            break;
+        case '/':
+            try
+            {
+                result = firstNumber / secondNumber;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division by zero");
+            }
+            break;
+        default:
+            Console.WriteLine("Error: unknown input");
+            break;
+    }
 }
-
+catch (FormatException)
+{
+    Console.WriteLine("Incorrect format");
+}
